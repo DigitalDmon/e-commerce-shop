567093588f7f232dd933493f2a01ef1db11a3755
DigitalDmon <161183806+DigitalDmon@users.noreply.github.com>
2024-10-22 03:22:36 -0500

    Solving errors 3

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import Link from "next/link"
-import { Menu, Search, ShoppingCart, User } from "lucide-react"
+import { Menu, ShoppingCart, User } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import {
     Sheet,
